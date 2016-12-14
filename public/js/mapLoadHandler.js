9bf17a01125c05d6dac1f517cc1d1d15b900b508
Scott <sbelan@uwo.ca>
2016-12-14 14:04:58 -0500

    Selecting certain countries in map now shows pictures below from that country. Set up page for showing programming languages. Hooked up links with new pages

---
+++ public/js/mapLoadHandler.js
@@ -0,0 +1,7 @@
+
+
+function loadPhotos (region) {
+    document.getElementById("image1").style.backgroundImage = "url('/images/" + region + "/image1.jpg')";
+    document.getElementById("image2").style.backgroundImage = "url('/images/" + region + "/image2.jpg')";
+    document.getElementById("image3").style.backgroundImage = "url('/images/" + region + "/image3.jpg')";
+}
