f5c29a154246a64de030207658456687c425439f
Scott <sbelan@uwo.ca>
2016-12-20 23:14:39 -0500

    Updated homepage with honeycomb grid system

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('./public/'));
+app.use(express.static(__dirname + '/public', {
+  extensions: ['html']
+}));
 
-app.listen(3000, function () {
+app.listen(8080, function () {
   console.log('Example app listening on port 3000!')
 })
